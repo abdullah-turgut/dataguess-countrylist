2b2f9b37d51473a6711e554c52d789a6afac76d5
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-09-12 18:33:50 +0300

    components created

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
+import { TbLoader2 } from 'react-icons/tb';
+import { BiErrorCircle } from 'react-icons/bi';
 
 const GET_COUNTRY_DATA = gql`
   query {
@@ -24,5 +25,50 @@ const GET_COUNTRY_DATA = gql`
 export default function DataTable() {
   const { loading, error, data } = useQuery(GET_COUNTRY_DATA);
 
-  return <div>DataTable</div>;
+  if (loading) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center flex-col gap-y-3">
+        <TbLoader2 className="animate-spin" size={40} />
+        <p className="text-sm">Fetching data...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center flex-col gap-y-3">
+        <BiErrorCircle size={40} />
+        <p className="text-sm">Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  console.log(data.countries);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Hello</th>
+            <th>Hello</th>
+            <th>Hello</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>World</td>
+            <td>World</td>
+            <td>World</td>
+          </tr>
+          <tr>
+            <td>World</td>
+            <td>World</td>
+            <td>World</td>
+          </tr>
+        </tbody>
+        <tfoot></tfoot>
+      </table>
+    </div>
+  );
 }
