a2775e4927ec62f15bcd11665b09780cf151817f
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-09-13 21:52:25 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <h1 className="text-3xl px-10 py-4 font-bold tracking-tighter">
-        DataGuess | Assessment
+        DataGuess | Assignment
       </h1>
       <DataTable />
     </div>
